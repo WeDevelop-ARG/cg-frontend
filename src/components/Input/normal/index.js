01510ec39b5cf5535fb6932afb475d6156c30d6a
Ricardo Rodriguez <tjrprecursor@gmail.com>
2020-04-17 22:23:24 +0200

    Add input password styles button

--- src/components/Input/normal/index.js
+++ src/components/Input/normal/index.js
@@ -1,34 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EyeOpen from '../../../vectors/eye.svg'
+import EyeOff from '../../../vectors/eye-off.svg'
+
+import './styles.scss'
 
 const InputNormal = ({
   name,
-  handleChange,
-  value,
-  touched,
-  errors,
+  value = '',
+  touched = {},
+  errors = {},
   isSubmitting = false,
-  handleBlur
+  isPassword = false,
+  ...props
 }) => {
+  const [isHidePassword, setIsHidePassword] = useState(true)
+
   const isTouched = () => {
     if (touched[name]) return 'input__normal--focused'
 
     return 'input__normal'
   }
+
+  const isShowPassword = () => {
+    if (isHidePassword) return 'password'
+
+    return 'text'
+  }
+
   return (
-    <>
+    <div className='input__normal--container'>
       <input
         className={
           errors[name] ? 'input__normal--error' : isTouched()
         }
         name={name}
-        type='text'
-        onChange={handleChange}
-        onBlur={handleBlur}
+        type={isPassword ? isShowPassword() : 'text'}
         value={value}
         disabled={isSubmitting}
+        {...props}
       />
+      {isPassword && <img onClick={() => setIsHidePassword(!isHidePassword)} className='input__normal__eye' src={isHidePassword ? EyeOff : EyeOpen} alt='' />}
       {errors[name] && touched[name] && <div className='input__normal__error-text'>{errors[name]}</div>}
-    </>
+    </div>
   )
 }
 
--- src/scenes/Signin/index.js
+++ src/scenes/Signin/index.js
@@ -1,10 +1,64 @@
 import React from 'react'
-import SigninForm from '../../components/Auth/SigninForm'
+import { useFormik } from 'formik'
+import Input from '../../components/Input'
+import * as Yup from 'yup'
+import ShapeAuth from '../../vectors/shape-auth.svg'
+import WomanShape from '../../vectors/woman.svg'
+// import SigninForm from '../../components/Auth/SigninForm'
+
+import './Signin.scss'
 
 const Signin = () => {
+  const SigninSchema = Yup.object().shape({
+    password: Yup.string()
+      .required('La contraseña es requerida'),
+    email: Yup.string()
+      .email('E-mail inválido')
+      .required('El E-mail es requerido')
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: values => {
+      window.alert(JSON.stringify(values, null, 2))
+    },
+    validationSchema: SigninSchema
+  })
+
+  console.log(formik)
+
   return (
-    <div>
-      <SigninForm />
+    <div className='signin--container'>
+      <div className='signin__form--container'>
+        <h2 className='signin--greetings'>¡HOLA!</h2>
+        <form className='signin__form'>
+          <label className='signin__form--labels'>Email</label>
+          <Input
+            name='email'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            errors={formik.errors}
+            touched={formik.touched}
+          />
+          <label className='signin__form--labels'>Password</label>
+          <Input
+            name='password'
+            isPassword
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            errors={formik.errors}
+            touched={formik.touched}
+          />
+        </form>
+      </div>
+      <div className='signin__shape' style={{ backgroundImage: `url(${ShapeAuth})` }}>
+        <img src={WomanShape} className='signin__shape__woman-shape' />
+      </div>
     </div>
   )
 }
