0cb517508d3e03d9bbe40d14ceb5a10b789bff37
Ricardo Rodriguez <tjrprecursor@gmail.com>
2020-04-17 23:46:09 +0200

    Add styles

--- src/scenes/Signin/index.js
+++ src/scenes/Signin/index.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useFormik } from 'formik'
 import Input from '../../components/Input'
 import * as Yup from 'yup'
 import ShapeAuth from '../../vectors/shape-auth.svg'
 import WomanShape from '../../vectors/woman.svg'
-// import SigninForm from '../../components/Auth/SigninForm'
+import { Link, useHistory } from 'react-router-dom'
+import Button from '../../components/Button'
+
+import useLogin from '../../hooks/useLoginMutation'
+import AuthContext from '../../Contexts/AuthContext/context'
 
 import './Signin.scss'
 
 const Signin = () => {
+  const { handleAuth } = useContext(AuthContext)
+  const { login } = useLogin()
+
+  const history = useHistory()
+
   const SigninSchema = Yup.object().shape({
     password: Yup.string()
       .required('La contraseña es requerida'),
@@ -22,19 +31,21 @@ const Signin = () => {
       email: '',
       password: ''
     },
-    onSubmit: values => {
-      window.alert(JSON.stringify(values, null, 2))
+    onSubmit: async ({ email, password }) => {
+      const { token } = await login({ email, password })
+
+      handleAuth(token)
+
+      await history.push('/')
     },
     validationSchema: SigninSchema
   })
-
-  console.log(formik)
 
   return (
     <div className='signin--container'>
       <div className='signin__form--container'>
         <h2 className='signin--greetings'>¡HOLA!</h2>
-        <form className='signin__form'>
+        <form className='signin__form' onSubmit={formik.handleSubmit}>
           <label className='signin__form--labels'>Email</label>
           <Input
             name='email'
@@ -44,7 +55,7 @@ const Signin = () => {
             errors={formik.errors}
             touched={formik.touched}
           />
-          <label className='signin__form--labels'>Password</label>
+          <label className='signin__form--labels'>Contraseña</label>
           <Input
             name='password'
             isPassword
@@ -54,6 +65,16 @@ const Signin = () => {
             errors={formik.errors}
             touched={formik.touched}
           />
+          <Link className='signin__form__links'>
+            Olvidaste tu contraseña?
+          </Link>
+          <Button type='submit'>Ingresar</Button>
+          <span className='signin__form__new'>
+            ¿Sos un usuario nuevo?
+            <Link className='signin__form__links' to='/auth/signup'>
+              Registrate
+            </Link>
+          </span>
         </form>
       </div>
       <div className='signin__shape' style={{ backgroundImage: `url(${ShapeAuth})` }}>
