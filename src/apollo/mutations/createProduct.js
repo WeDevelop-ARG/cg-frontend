dd185faa6dd9b8620f826f4204cfc9c5a4836674
Ricardo Rodriguez <tjrprecursor@gmail.com>
2020-04-20 15:35:29 +0200

    Add product fragment to create product mutation

--- src/apollo/mutations/createProduct.js
+++ src/apollo/mutations/createProduct.js
@@ -4,12 +4,7 @@ import productFragment from '../fragments/product'
 export default gql`
   mutation createProduct($productInput: ProductInput!) {
     createProduct(input: $productInput) {
-      id
-      name
-      description
-      photoUrl
-      price
-      marketPrice
+      ...product
     }
   }
   ${productFragment}
