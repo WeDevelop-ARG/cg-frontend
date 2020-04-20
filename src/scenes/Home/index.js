6ca0cc4a3f4180b74caf9b13c8084825387bb605
Ezex360 <eigiachero@gmail.com>
2020-04-20 11:54:16 -0300

    Filter groups by size

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -10,30 +10,59 @@ import Banner from '../../components/HomeBanner'
 const Landing = (props) => {
   const { groups } = useGroupsQuery()
 
+  const SIZE_FILTER = 8
+  const parGroup = []
+  const smallGroup = []
+  const bigGroup = []
+
+  groups.forEach(group => {
+    if (group.type === 'GROUP') {
+      return group.minParticipants <= SIZE_FILTER ? smallGroup.push(group) : bigGroup.push(group)
+    }
+
+    return parGroup.push(group)
+  })
+
+  const getGroupCard = (group) => {
+    return (
+      <GroupCard
+        key={group.id}
+        productPicture={group.product.photoUrl}
+        marketPrice={group.product.marketPrice}
+        price={group.product.price}
+        minParticipants={group.minParticipants}
+        currentParticipants={group.participantsCount}
+        expiresAt={group.expiresAt}
+        productId={group.product.id}
+        groupId={group.id}
+        isSubscribed={group.isSubscribed}
+        colors={['Azul', 'Rojo', 'Negro']} // TODO this must come from backend
+        title={group.product.name}
+        description={group.product.description}
+      />
+    )
+  }
+
   return (
     <>
       <Banner />
       <div className='Landing'>
-        <h2 className='Landing--category-title'>Los más populares</h2>
+        <h2 className='Landing--category-title'>Compra en dúo</h2>
         <div className='Landing__main-container'>
           {
-            groups.map((group) => (
-              <GroupCard
-                key={group.id}
-                productPicture={group.product.photoUrl}
-                marketPrice={group.product.marketPrice}
-                price={group.product.price}
-                minParticipants={group.minParticipants}
-                currentParticipants={group.participantsCount}
-                expiresAt={group.expiresAt}
-                productId={group.product.id}
-                groupId={group.id}
-                isSubscribed={group.isSubscribed}
-                colors={['Azul', 'Rojo', 'Negro']} // TODO this must come from backend
-                title={group.product.name}
-                description={group.product.description}
-              />
-            ))
+            parGroup.map(group => getGroupCard(group))
+          }
+        </div>
+        <h2 className='Landing--category-title'>Grupos pequeños</h2>
+        <div className='Landing__main-container'>
+          {
+            smallGroup.map(group => getGroupCard(group))
+          }
+        </div>
+        <h2 className='Landing--category-title'>Grupos grandes</h2>
+        <div className='Landing__main-container'>
+          {
+            bigGroup.map(group => getGroupCard(group))
           }
         </div>
       </div>
