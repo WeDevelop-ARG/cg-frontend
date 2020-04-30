0cb2cbe08569223d0a21e14b17ae1aff6eaec900
Ezequiel <eigiachero@gmail.com>
2020-04-30 17:04:25 -0300

    Add redirect to seller landing (#87)
    * Add redirect to seller landing
    
    * Fix issues

--- src/scenes/SellerLanding/index.js
+++ src/scenes/SellerLanding/index.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import LandingA from '../Landing'
 import LandingB from '../LandingV2'
-import * as firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/remote-config'
+
+const queryParams = () => {
+  return new URLSearchParams(useLocation().search)
+}
 
 const SellerLanding = () => {
   const [Landing, setLanding] = useState()
+  const redirect = queryParams().get('useLanding')
 
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
+    if (redirect) {
+      setLanding(redirect)
+    } else if (process.env.NODE_ENV === 'production') {
       const remote = firebase.remoteConfig()
       remote.fetchAndActivate().then(() => {
         const whichLanding = remote.getString('seller_landing_page')
