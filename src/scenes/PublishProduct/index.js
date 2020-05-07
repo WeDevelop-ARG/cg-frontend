245ee17f280a92aeb96205fcc4a10ad45c66ea96
Ricardo Andres <tjrprecursor@gmail.com>
2020-05-07 17:02:18 +0200

    confirmantion modal (#100)
    * confirmation modal
    
    * Modal styles fixed

--- src/scenes/PublishProduct/index.js
+++ src/scenes/PublishProduct/index.js
@@ -1,20 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import ConfirmModal from '../../components/ConfirmModal'
 
 import MultiStepForm from './MultiStepForm'
 import './PublishProduct.scss'
 
 const PublishProduct = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const history = useHistory()
+
+  const goToMain = () => {
+    return history.push('/')
+  }
+
   return (
-    <div className='Publish'>
-      <Link to='/' className='Publish__go-home'>
-        &#60;  Volver a la pagina principal
-      </Link>
-      <h1>Publicar Producto</h1>
-      <div className='Publish--container'>
-        <MultiStepForm className='Publish__ms-form' />
+    <>
+      {
+        isModalOpen && (
+          <ConfirmModal
+            confirmMessage='Continuar con la carga'
+            rejectMessage='Salir de todas formas'
+            title='Salir del proceso de carga'
+            description='Tené en cuenta que si salís del proceso de carga, perderás la información del producto que añadiste.'
+            onClose={() => setIsModalOpen(false)}
+            onConfirm={() => setIsModalOpen(false)}
+            onReject={goToMain}
+          />
+        )
+      }
+      <div className='Publish'>
+        <a onClick={() => setIsModalOpen(true)} className='Publish__go-home'>
+          &#60;  Volver a la pagina principal
+        </a>
+        <h1>Publicar Producto</h1>
+        <div className='Publish--container'>
+          <MultiStepForm className='Publish__ms-form' />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
