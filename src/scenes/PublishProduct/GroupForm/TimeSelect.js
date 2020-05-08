5e25a69fcc0380afc7657e60b4e46082ce7e2fb7
Ezequiel <eigiachero@gmail.com>
2020-05-08 09:26:04 -0300

    Control timepicker input (#112)

--- src/scenes/PublishProduct/GroupForm/TimeSelect.js
+++ src/scenes/PublishProduct/GroupForm/TimeSelect.js
@@ -4,27 +4,46 @@ import classes from './time.module.scss'
 
 const TimeSelect = ({ setTimeString }) => {
   const [time, setTime] = useState({ hours: '11', minutes: '59', zone: 'PM' })
-  const buttonClass = (select) => {
-    return select === time.zone ? classes.buttonNot : classes.buttonYes
-  }
-  const handleChange = (event) => {
-    setTime({ ...time, [event.target.name]: event.target.value })
-  }
-  const dontSubmit = (event) => {
-    if (event.which === 13 /* Enter */) {
-      event.preventDefault()
-    }
-  }
   useEffect(() => {
     const timeString = time.hours + ':' + time.minutes + ' ' + time.zone
     setTimeString(timeString)
   })
 
+  const buttonClass = (select) => {
+    return select === time.zone ? classes.buttonNot : classes.buttonYes
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name
+    const OldValue = event.target.value
+
+    if (name === 'hours') event.target.value = OldValue < 0 ? '' : OldValue > 12 ? 12 : OldValue
+    if (name === 'minutes') event.target.value = OldValue < 0 ? '' : OldValue > 59 ? 59 : OldValue
+
+    setTime({ ...time, [name]: event.target.value })
+  }
+
+  const handleBlur = (event) => {
+    const name = event.target.name
+    const OldValue = event.target.value
+
+    if (name === 'hours' && (OldValue === '' || OldValue < 1)) event.target.value = 11
+    if (name === 'minutes' && OldValue === '') event.target.value = 59
+
+    setTime({ ...time, [name]: event.target.value })
+  }
+
+  const dontSubmit = (event) => {
+    if (event.which === 13 /* Enter */) {
+      event.preventDefault()
+    }
+  }
+
   return (
     <div className={classes.container}>
-      <input type='number' autoComplete='off' placeholder='11' name='hours' onInput={handleChange} onKeyPress={dontSubmit} />
+      <input type='number' autoComplete='off' placeholder='11' name='hours' onInput={handleChange} onBlur={handleBlur} onKeyPress={dontSubmit} />
       :
-      <input type='number' autoComplete='off' placeholder='59' name='minutes' onInput={handleChange} onKeyPress={dontSubmit} />
+      <input type='number' autoComplete='off' placeholder='59' name='minutes' onInput={handleChange} onBlur={handleBlur} onKeyPress={dontSubmit} />
       <button className={buttonClass('AM')} name='zone' value='AM' onClick={handleChange} type='button'>AM</button>
       <button className={buttonClass('PM')} name='zone' value='PM' onClick={handleChange} type='button'>PM</button>
     </div>
