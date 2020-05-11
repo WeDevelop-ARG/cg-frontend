13d749966aab65067944b1f73b71e909e52026f6
Ezequiel <eigiachero@gmail.com>
2020-05-11 15:18:53 -0300

    change a to Link in LandingA (#130)

--- src/scenes/Landing/FirstStep/index.js
+++ src/scenes/Landing/FirstStep/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import classes from './styles.module.scss'
 
 const FirstStep = (props) => {
@@ -8,9 +9,9 @@ const FirstStep = (props) => {
         <span className={classes.you}>VOS</span>
         <span className={classes.loadProducts}>Cargá tus productos</span>
         <span className={classes.register}>
-          <a id='seller_landing_a_cta_top' href='/auth/signup' className={classes.registerRed}>
+          <Link id='seller_landing_a_cta_top' to='/auth/signup' className={classes.registerRed}>
             Registrate
-          </a> con tu email y comenzá a cargar tus productos en nuestra plataforma
+          </Link> con tu email y comenzá a cargar tus productos en nuestra plataforma
         </span>
       </div>
     </div>
