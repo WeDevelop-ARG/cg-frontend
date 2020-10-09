8d37d76e579a21d57fb43b541437c4e925e835e2
MPucheta <smpucheta@gmail.com>
2020-10-09 12:51:43 -0300

    Remove Text component

--- src/modules/MainApp/components/Subtitle/index.js
+++ src/modules/MainApp/components/Subtitle/index.js
@@ -1,7 +1,6 @@
 import React, { useMemo } from 'react'
+import PropTypes from 'prop-types'
 import classnames from 'classnames'
-
-import Text from '../Text'
 
 import classes from './styles.module.scss'
 
@@ -9,12 +8,13 @@ const Subtitle = ({ className, children, ...restProps }) => {
   const subtitleClassnames = useMemo(() => classnames(classes.subtitle, className), [className])
 
   return (
-    <Text className={subtitleClassnames} {...restProps}>{children}</Text>
+    <div className={subtitleClassnames} {...restProps}>{children}</div>
   )
 }
 
 Subtitle.propTypes = {
-  ...Text.propTypes
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  children: PropTypes.node
 }
 
 export default Subtitle
--- src/modules/MainApp/components/Title/index.js
+++ src/modules/MainApp/components/Title/index.js
@@ -1,7 +1,6 @@
 import React, { useMemo } from 'react'
+import PropTypes from 'prop-types'
 import classnames from 'classnames'
-
-import Text from '../Text'
 
 import classes from './styles.module.scss'
 
@@ -9,12 +8,13 @@ const Title = ({ className, children, ...restProps }) => {
   const titleClassnames = useMemo(() => classnames(classes.title, className), [className])
 
   return (
-    <Text className={titleClassnames} {...restProps}>{children}</Text>
+    <div className={titleClassnames} {...restProps}>{children}</div>
   )
 }
 
 Title.propTypes = {
-  ...Text.propTypes
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  children: PropTypes.node
 }
 
 export default Title
